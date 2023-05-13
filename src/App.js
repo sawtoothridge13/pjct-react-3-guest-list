670e217ebec4cea1123a86e622da5f65ba5fae46
Doug Andrews <sawtoothridge13@gmail.com>
2023-05-13 16:18:12 +0200

    Add attending status checkbox

--- src/App.js
+++ src/App.js
@@ -24,21 +24,49 @@ export default function App() {
   const response = await fetch(`${baseUrl}/guests/1`, { method: 'DELETE' });
 const deletedGuest = await response.json(); */
 
+  // 1. Create state for input data
+  const [isCookiePolicyAccepted, setIsCookiePolicyAccepted] = useState(false);
+
   return (
-    <div className={styles.App}>
+    <div className={styles.App} data-test-id="guest">
       <header>
         <h1>Guest List App</h1>
       </header>
-      <form>
-        <label>
-          First name
-          <input />
-        </label>
-        <label>
-          Last name
-          <input />
-        </label>
-      </form>
+
+      <section className={styles.section}>
+        <form>
+          <label>
+            First name
+            <input />
+          </label>
+        </form>
+        <form>
+          <label>
+            Last name
+            <input />
+            {/* <kbd>Return</kbd> */}
+          </label>
+        </form>
+      </section>
+
+      <section>
+        <h2>Guests</h2>
+        <form>
+          {isCookiePolicyAccepted}
+          <label aria-label="attending status">
+            Attending
+            {/* // 2. use the current 'checked' (only for checkboxes, otherwise 'value') of the state as the value of the input */}
+            <input
+              checked={isCookiePolicyAccepted}
+              type="checkbox"
+              // 3. update the state value with the event.currentTarget.value
+              onChange={(event) => {
+                setIsCookiePolicyAccepted(event.currentTarget.checked);
+              }}
+            />
+          </label>
+        </form>
+      </section>
     </div>
   );
 }
