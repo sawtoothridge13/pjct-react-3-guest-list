80456d0506c59b91eb8944c16811216ed34ea9c0
Doug Andrews <sawtoothridge13@gmail.com>
2023-05-13 16:57:28 +0200

    Created one form from multiple forms

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import styles from './App.module.scss';
 
 export default function App() {
   /*   const baseUrl = 'http://localhost:4000';
+
   const response = await fetch(`${baseUrl}/guests`);
+
   const allGuests = await response.json();
+
   const response = await fetch(`${baseUrl}/guests`, {
     method: 'POST',
     headers: {
@@ -12,7 +15,9 @@ export default function App() {
     },
     body: JSON.stringify({ firstName: 'Karl', lastName: 'Horky' }),
   });
+
   const createdGuest = await response.json();
+
   const response = await fetch(`${baseUrl}/guests/1`, {
     method: 'PUT',
     headers: {
@@ -20,53 +25,57 @@ export default function App() {
     },
     body: JSON.stringify({ attending: true }),
   });
+
   const updatedGuest = await response.json();
+
   const response = await fetch(`${baseUrl}/guests/1`, { method: 'DELETE' });
+
 const deletedGuest = await response.json(); */
 
   // 1. Create state for input data
-  const [isCookiePolicyAccepted, setIsCookiePolicyAccepted] = useState(false);
+  const [attendingStatus, setAttendingStatus] = useState(false);
+
+  // don't use document.querySelector or document.getElementById
 
   return (
-    <div className={styles.App} data-test-id="guest">
+    <div className={styles.App}>
       <header>
         <h1>Guest List App</h1>
       </header>
 
-      <section className={styles.section}>
-        <form>
-          <label>
-            First name
-            <input />
-          </label>
-        </form>
-        <form>
-          <label>
-            Last name
-            <input />
-            {/* <kbd>Return</kbd> */}
-          </label>
-        </form>
-      </section>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <label>
+          First name
+          <input />
+        </label>
+        <br />
+        <label>
+          Last name
+          <input />
+          {/* <kbd>Return</kbd> */}
+        </label>
 
-      <section>
         <h2>Guests</h2>
-        <form>
-          {isCookiePolicyAccepted}
-          <label aria-label="attending status">
-            Attending
-            {/* // 2. use the current 'checked' (only for checkboxes, otherwise 'value') of the state as the value of the input */}
-            <input
-              checked={isCookiePolicyAccepted}
-              type="checkbox"
-              // 3. update the state value with the event.currentTarget.value
-              onChange={(event) => {
-                setIsCookiePolicyAccepted(event.currentTarget.checked);
-              }}
-            />
-          </label>
-        </form>
-      </section>
+
+        {attendingStatus}
+        <label aria-label="attending status">
+          Attending
+          {/* // 2. use the current 'checked' (only for checkboxes, otherwise 'value') of the state as the value of the input */}
+          <input
+            checked={attendingStatus}
+            type="checkbox"
+            // 3. update the state value with the event.currentTarget.value
+            onChange={(event) => {
+              setAttendingStatus(event.currentTarget.checked);
+            }}
+          />
+        </label>
+        <button>Test</button>
+      </form>
     </div>
   );
 }
