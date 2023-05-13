1525d8ce5bbeb3f3672ccf71582d50b43283f9d5
Doug Andrews <sawtoothridge13@gmail.com>
2023-05-13 10:42:10 +0200

    Replace App.js code with controlled components exa mple template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.scss';
 
 export default function App() {
-  return <div className="App">Hello</div>;
+  const [label, setLabel] = useState('');
+  // 1. Create the state variable
+  const [usernameInput, setUsernameInput] = useState('');
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      {/* Boolean operator */}
+      {label !== '' && <p>Username: {label}</p>}
+      {/* 2. Connect the state with the input value */}
+      <input
+        value={usernameInput}
+        onChange={(event) => {
+          // 3. update state on change event
+          setUsernameInput(event.currentTarget.value);
+        }}
+      />
+      {/* Ternary operator */}
+      {usernameInput === '' ? (
+        ' please type ⌨️'
+      ) : (
+        <button
+          onClick={() => {
+            setLabel(usernameInput);
+          }}
+        >
+          update label
+        </button>
+      )}
+    </form>
+  );
 }
