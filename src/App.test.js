90a12ba3d01db6c38436df4c9a42723b9dc6a6c0
Doug Andrews <sawtoothridge13@gmail.com>
2023-05-16 17:52:53 +0200

    Add functions for fetchGuest, newGuest, handleChange, and handleKeyDown

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { expect, test } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
 
 test('renders learn react link', () => {
   render(<App />);
